32c535c37d2847b72fd854b7e8f13ed936cd30eb
Keshav Chakravarthy <keshav1985@gmail.com>
2022-07-12 18:15:24 +0530

    fib explanation

--- spawn/fib.exs
+++ spawn/fib.exs
@@ -27,20 +27,32 @@ defmodule Scheduler do
 
   defp schedule_processes(processes, queue, results) do
     receive do
+      # The flow is this: When the scheduler gets a ready message from a fib-server
+      # the top number in queue is sent to it to be calculated (fib value). In the scheduler
+      # we don't keep track of which fib-server is occupied etc. The process is driven entirely
+      # by the fib-server messages.
       {:ready, pid} when queue != [] ->
         [next | tail] = queue
         send(pid, {:fib, next, self()})
         schedule_processes(processes, tail, results)
 
+      # when the to_process queue is empty, and a fib-server says it's ready for the next calculation
+      # we send the shutdown signal to that fib-server. At this point, all calculation has been done
+      # so shutdown signal is sent to any process that is remaining. If the fib-server is calculating a
+      # value, then after it sends the result back to the scheduler, it goes into receiving mode and
+      # then receives the shutdown signal that was sent to it, while it was busy calculating.
       {:ready, pid} ->
         send(pid, {:shutdown})
 
         if length(processes) > 1 do
           schedule_processes(List.delete(processes, pid), queue, results)
         else
+          # results are sorted because they are added in the order we get them
           Enum.sort(results, fn {n1, _}, {n2, _} -> n1 <= n2 end)
         end
 
+      # On receiving an answer, the result is stored in the results array and the function is
+      # recursively called with the current results
       {:answer, number, result, _pid} ->
         # results added in the order we get them
         schedule_processes(processes, queue, [{number, result} | results])
