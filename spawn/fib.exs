b22cf95d5389c7f351154140b5e43644302e178a
Keshav Chakravarthy <keshav1985@gmail.com>
2022-07-12 18:01:49 +0530

    finish fib

--- spawn/fib.exs
+++ spawn/fib.exs
@@ -1,35 +1,35 @@
 defmodule FibSolver do
   # This is the fib server
   def fib(scheduler) do
-    send(scheduler, { :ready, self() })
+    send(scheduler, {:ready, self()})
 
     receive do
-      { :fib, n, client } ->
-        send(client, { :answer, n, fib_calc(n), self() })
+      {:fib, n, client} ->
+        send(client, {:answer, n, fib_calc(n), self()})
         fib(scheduler)
 
-      { :shutdown } ->
+      {:shutdown} ->
         exit(:normal)
     end
   end
 
   defp fib_calc(0), do: 0
   defp fib_calc(1), do: 1
-  defp fib_calc(n), do: fib_calc(n-1) + fib_calc(n-2)
+  defp fib_calc(n), do: fib_calc(n - 1) + fib_calc(n - 2)
 end
 
 defmodule Scheduler do
   def run(num_processes, module, func, to_calculate) do
-    (1..num_processes)
-    |> Enum.map(fn(_) -> spawn(module, func, [self()]) end)
+    1..num_processes
+    |> Enum.map(fn _ -> spawn(module, func, [self()]) end)
     |> schedule_processes(to_calculate, [])
   end
 
   defp schedule_processes(processes, queue, results) do
     receive do
       {:ready, pid} when queue != [] ->
-        [ next | tail ] = queue
-        send(pid, { :fib, next, self() })
+        [next | tail] = queue
+        send(pid, {:fib, next, self()})
         schedule_processes(processes, tail, results)
 
       {:ready, pid} ->
@@ -42,19 +42,22 @@ defmodule Scheduler do
         end
 
       {:answer, number, result, _pid} ->
-        schedule_processes(processes, queue, [ {number, result} | results ])
+        # results added in the order we get them
+        schedule_processes(processes, queue, [{number, result} | results])
     end
   end
 end
 
-to_calculate = List.duplicate(37, 20)
+# to_calculate = List.duplicate(37, 20)
+to_calculate = Enum.to_list(1..37)
 
-Enum.each 1..10, fn num_processes ->
+Enum.each(1..10, fn num_processes ->
   {time, result} = :timer.tc(Scheduler, :run, [num_processes, FibSolver, :fib, to_calculate])
 
   if num_processes == 1 do
-    IO.puts inspect result
-    IO.puts "\n #   time (s)"
+    IO.puts(inspect(result))
+    IO.puts("\n #   time (s)")
   end
+
   :io.format("~2B     ~.2f~n", [num_processes, time / 1_000_000.0])
-end
+end)
